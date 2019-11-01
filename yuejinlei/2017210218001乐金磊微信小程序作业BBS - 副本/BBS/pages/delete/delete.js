dcd2cc994ac303d889e514e1da1ac48200c5220b
unknown <2437381590@qq,com>
2019-11-01 09:29:28 +0800

    yuijinlei第四次作业
    yuijinlei第四次作业

---
+++ yuejinlei/2017210218001乐金磊微信小程序作业BBS - 副本/BBS/pages/delete/delete.js
@@ -0,0 +1,98 @@
+// pages/delete/delete.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  pids:'',
+  },
+  bindpid: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      pid: e.detail.value
+    })
+  },
+  
+  
+  clickdelete: function () {
+    wx.request({
+      url: 'http://172.22.130.33/index.php/Ajaxapi/Ajaxapi/deleteproblembyid/userid/' + app.globalData.userid + '/pid/' + this.data.pid,
+      success: function (res) {
+        if (res.data.info == 1) {
+          wx.showToast({
+            title: '成功',
+            icon: 'none',
+            duration: 80000,
+          })
+          wx.navigateTo({
+            url: '/pages/luntan/luntan',
+          })
+        } else {
+          wx.showToast({
+            title: 'chucuo',
+            icon: 'none',
+            duration: 8000
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
