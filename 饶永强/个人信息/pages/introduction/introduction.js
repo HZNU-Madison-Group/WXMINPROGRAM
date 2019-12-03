7f2002a3ab9c89b626b50c6aa28f0e382067092c
ryq19990425 <55565742+ryq19990425@users.noreply.github.com>
2019-12-03 18:03:32 +0800

    个人信息

---
+++ 饶永强/个人信息/pages/introduction/introduction.js
@@ -0,0 +1,29 @@
+// pages/introduction/introduction.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  
+
+  },
+  goToTalkPage: function(param) {
+    wx.navigateTo({
+
+      url: '/pages/xueyuan/xueyuan',
+
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+ 
+ 
+
+})
